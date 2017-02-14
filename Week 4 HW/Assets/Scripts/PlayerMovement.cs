84bd285b469884ff142b7225f69b8652f50bed40
Unknown <andrewpetersell@gmail.com>
2017-02-14 08:28:40 -0500

    Player Movement Working

---
+++ Week 4 HW/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode left;
+	public KeyCode right;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveBabyMove (up, Vector2.up);
+		moveBabyMove (down, Vector2.down);
+		moveBabyMove (right, Vector2.right);
+		moveBabyMove (left, Vector2.left); 
+		
+	}
+
+	void moveBabyMove (KeyCode key, Vector2 direction)
+	{
+		if (Input.GetKey(key)) 
+		{
+			transform.Translate( direction * moveSpeed * Time.deltaTime);
+		}
+				
+	}
+}
